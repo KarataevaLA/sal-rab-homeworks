462f79b44ea712530a47658408ae1f568ff3058f
Lyubov Karataeva <karataeva.la@gmail.com>
2022-04-21 21:25:53 +0300

    task 2 completed + task 3 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -9,12 +9,10 @@ function calcShipping(sum, min, shipping) {
     let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-    if (productsSum == 0) {
-        shippingSum = 0;
-    }
+    
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-    if (productsSum >= freeShippingMinSum) {
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
     }
     // если productsSum больше 0 и меньше freeShippingMinSum,
@@ -35,15 +33,11 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
-    let discountSum;
+    
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-    if (productsSum >= discountMinSum) {
-        discountSum = discountPart * productsSum / 100;
-    } else {
-        discountSum = 0;
-    }
+    let discountSum = productsSum >= discountMinSum ? discountPart * productsSum / 100 : 0;
     // Конец решения задания №2.2.
 
     return discountSum;
